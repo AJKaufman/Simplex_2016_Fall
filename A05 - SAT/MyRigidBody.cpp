21aa6b8c04ccabe4af835dc7ab86080db5707477
MAIN\ak3012 <ak3012>
2017-11-15 13:58:50 -0500

    started working on SAT

--- A05 - SAT/MyRigidBody.cpp
+++ A05 - SAT/MyRigidBody.cpp
@@ -4,9 +4,9 @@ using namespace Simplex;
 void MyRigidBody::Init(void)
 {
 	m_pMeshMngr = MeshManager::GetInstance();
-	m_bVisibleBS = false;
+	m_bVisibleBS = true;
 	m_bVisibleOBB = true;
-	m_bVisibleARBB = false;
+	m_bVisibleARBB = true;
 
 	m_fRadius = 0.0f;
 
@@ -285,7 +285,88 @@ uint MyRigidBody::SAT(MyRigidBody* const a_pOther)
 	find a separating axis you need to return 0, there is an enum in
 	Simplex that might help you [eSATResults] feel free to use it.
 	(eSATResults::SAT_NONE has a value of 0)
-	*/
+	*/	
+
+	float ra, rb;
+	glm::vec2* R;
+	glm::vec2* AbsR;
+
+	// Compute rotation matrix expressing a_pOther in a's coordinate frame
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			R[i][j] = glm::dot(this.u[i], a_pOther.u[j]);
+
+	// Compute translation vector t
+	Vector t = a_pOther.c - a.c;
+	// Bring translation into a's coordinate frame
+	t = Vector(Dot(t, a.u[0]), Dot(t, a.u[2]), Dot(t, a.u[2]));
+
+	// Compute common subexpressions. Add in an epsilon term to
+	// counteract arithmetic errors when two edges are parallel and
+	// their cross product is (near) null (see text for details)
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			AbsR[i][j] = Abs(R[i][j]) + EPSILON;
+
+	// Test axes L = A0, L = A1, L = A2
+	for (int i = 0; i < 3; i++) {
+		ra = a.e[i];
+		rb = a_pOther.e[0] * AbsR[i][0] + a_pOther.e[1] * AbsR[i][1] + a_pOther.e[2] * AbsR[i][2];
+		if (Abs(t[i]) > ra + rb) return 0;
+	}
+
+	// Test axes L = B0, L = B1, L = B2
+	for (int i = 0; i < 3; i++) {
+		ra = a.e[0] * AbsR[0][i] + a.e[1] * AbsR[1][i] + a.e[2] * AbsR[2][i];
+		rb = a_pOther.e[i];
+		if (Abs(t[0] * R[0][i] + t[1] * R[1][i] + t[2] * R[2][i]) > ra + rb) return 0;
+	}
+
+	// Test axis L = A0 x B0
+	ra = a.e[1] * AbsR[2][0] + a.e[2] * AbsR[1][0];
+	rb = a_pOther.e[1] * AbsR[0][2] + a_pOther.e[2] * AbsR[0][1];
+	if (Abs(t[2] * R[1][0] - t[1] * R[2][0]) > ra + rb) return 0;
+
+	// Test axis L = A0 x B1
+	ra = a.e[1] * AbsR[2][1] + a.e[2] * AbsR[1][1];
+	rb = a_pOther.e[0] * AbsR[0][2] + a_pOther.e[2] * AbsR[0][0];
+	if (Abs(t[2] * R[1][1] - t[1] * R[2][1]) > ra + rb) return 0;
+
+	// Test axis L = A0 x B2
+	ra = a.e[1] * AbsR[2][2] + a.e[2] * AbsR[1][2];
+	rb = a_pOther.e[0] * AbsR[0][1] + a_pOther.e[1] * AbsR[0][0];
+	if (Abs(t[2] * R[1][2] - t[1] * R[2][2]) > ra + rb) return 0;
+
+	// Test axis L = A1 x B0
+	ra = a.e[0] * AbsR[2][0] + a.e[2] * AbsR[0][0];
+	rb = a_pOther.e[1] * AbsR[1][2] + a_pOther.e[2] * AbsR[1][1];
+
+	if (Abs(t[0] * R[2][0] - t[2] * R[0][0]) > ra + rb) return 0;
+
+	// Test axis L = A1 x B1
+	ra = a.e[0] * AbsR[2][1] + a.e[2] * AbsR[0][1];
+	rb = a_pOther.e[0] * AbsR[1][2] + a_pOther.e[2] * AbsR[1][0];
+	if (Abs(t[0] * R[2][1] - t[2] * R[0][1]) > ra + rb) return 0;
+
+	// Test axis L = A1 x B2
+	ra = a.e[0] * AbsR[2][2] + a.e[2] * AbsR[0][2];
+	rb = a_pOther.e[0] * AbsR[1][1] + a_pOther.e[1] * AbsR[1][0];
+	if (Abs(t[0] * R[2][2] - t[2] * R[0][2]) > ra + rb) return 0;
+
+	// Test axis L = A2 x B0
+	ra = a.e[0] * AbsR[1][0] + a.e[1] * AbsR[0][0];
+	rb = a_pOther.e[1] * AbsR[2][2] + a_pOther.e[2] * AbsR[2][1];
+	if (Abs(t[1] * R[0][0] - t[0] * R[1][0]) > ra + rb) return 0;
+
+	// Test axis L = A2 x B1
+	ra = a.e[0] * AbsR[1][1] + a.e[1] * AbsR[0][1];
+	rb = a_pOther.e[0] * AbsR[2][2] + a_pOther.e[2] * AbsR[2][0];
+	if (Abs(t[1] * R[0][1] - t[0] * R[1][1]) > ra + rb) return 0;
+
+	// Test axis L = A2 x B2
+	ra = a.e[0] * AbsR[1][2] + a.e[1] * AbsR[0][2];
+	rb = a_pOther.e[0] * AbsR[2][1] + a_pOther.e[1] * AbsR[2][0];
+	if (Abs(t[1] * R[0][2] - t[0] * R[1][2]) > ra + rb) return 0;
 
 	//there is no axis test that separates this two objects
 	return eSATResults::SAT_NONE;
